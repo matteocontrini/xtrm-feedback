1e10481647e79d63d400853364679d42ccf4b754
Matteo Contrini <m.contrini@gmail.com>
2014-12-08 18:36:50 +0100

    Empty Spark program

---
+++ xtrm-spark-client/xtrm-spark-client.cpp
@@ -0,0 +1,20 @@
+/*
+
+  XTRM feedback system
+  Spark device client
+
+  This program detects button click and
+  sends data to the server through TCP
+
+  Using - Spark Core
+        - 4 buttons
+
+ */
+
+void setup() {
+  // TODO everything
+}
+
+void loop() {
+  // TODO everything
+}
