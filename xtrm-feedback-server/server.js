1e4648c06f3614f0304c59b167f743d6f0e8b973
Matteo Contrini <m.contrini@gmail.com>
2014-12-08 18:36:36 +0100

    Feedback server 0.2.0

--- xtrm-feedback-server/server.js
+++ xtrm-feedback-server/server.js
@@ -1,4 +1,4 @@
-console.log('**** RUNNING ****');
+console.log('**** RUNNING ****\n');
 
 // Setup
 var net = require('net'); // socket
@@ -13,31 +13,48 @@ var httpServerPort = 3000;
 var socketPort = 3001;
 
 // TCP server
-// Listens for data coming from clients (Spark, Arduino)
+// Listens for data coming from clients (Spark, Arduino, shell)
 var tcpServer = net.createServer();
+
 tcpServer.on('connection', function onConnection(socket) {
-	socket.on('data', function (message) {
-		// Split the payload es. sparkId;buttonId
-		messageString = message.toString().trim();
-		data = messageString.split(';');
-		console.log('TCP SERVER --> Got ' +
+	console.log('SERVER --> New connection ' + socket.remoteAddress);
+	
+	// New message received
+	socket.on('data', function onData(message) {
+		console.log();
+		
+		// Split the payload es. deviceId;buttonId
+		var messageString = message.toString().trim();
+		
+		if (messageString == "") {
+			return;
+		};
+		var data = messageString.split(';');
+		console.log('SERVER --> Got ' +
 			data[1] + ' from ' + data[0]);
 		
 		buttonId = parseInt(data[1]);
 		// Check button ID validity
 		if (buttonId >= 0 && buttonId <= 3) {
 			// Forward message to the browser client
-			console.log('WEB BROWSER SOCKET --> Got valid data, forwarding');
+			console.log('WEB    --> Got valid data, forwarding');
 			io.emit('buttonPressed', messageString);
 		}
 		else {
-			console.log("SERVER --> Got invalid data")
+			console.log('SERVER --> Got invalid data');
 		}
 	});
+	
+	// Disconnection
+	socket.on('end', function onEnd() {
+		console.log('\nSERVER --> Client disconnected');
+	});
 });
+
 tcpServer.on('listening', function onListening() {
-	console.log('TCP SERVER --> Listening on port ' + socketPort);
+	console.log('SERVER --> Listening on port ' + socketPort);
 });
+
 // Start listening
 tcpServer.listen(socketPort);
 
@@ -46,14 +63,14 @@ tcpServer.listen(socketPort);
 // Listen for main page GET requests
 expressApp.get('/', function(req, res){
 	res.sendFile(__dirname + '/html/index.html');
-	console.log('WEB SERVER --> Main page request');
+	console.log('WEB    --> Request');
 });
 expressApp.use(express.static('html'));
 
 // Web server
 // Start listening
 httpServer.on('listening', function onListening() {
-	console.log('WEB SERVER --> Listening on port ' + httpServerPort);
+	console.log('WEB    --> Listening on port ' + httpServerPort);
 	console.log("\nLaunching the browser...\n");
 	open("http://localhost:" + httpServerPort);
 });
@@ -62,10 +79,9 @@ httpServer.listen(httpServerPort);
 // Web socket
 // Listen for connection
 io.on('connection', function onConnection(socket) {
-	console.log('WEB BROWSER SOCKET --> Client connected');
+	console.log('WEB    --> Client connected');
 
 	socket.on('disconnect', function onDisconnection() {
-		console.log('WEB BROWSER SOCKET --> Client disconnected');
+		console.log('WEB    --> Client disconnected');
 	});
 });
-console.log('WEB BROWSER SOCKET --> Listening');
